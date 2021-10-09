8329e0422e3563b672680960f8761d54ae143fe4
tekadabdul12 <tekadabdul12@gmail.com>
2021-10-10 00:58:57 +0700

    problem 4

---
+++ Assets/script/MoveInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInput : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    
+}
