640cd6d9afe7b398f9a618d57cadb3666857f2b0
tekadabdul12 <tekadabdul12@gmail.com>
2021-10-10 00:24:05 +0700

    problem 3

---
+++ Assets/script/MoveDot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDot : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float kecepatan;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Gerak();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Gerak()
+    {
+
+        //rb.velocity = new Vector2(kecepatan, kecepatan);
+
+        rb.AddForce(new Vector2 (kecepatan,kecepatan));
+    }
+}
