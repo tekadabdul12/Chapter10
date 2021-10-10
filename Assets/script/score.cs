bbec6cc5b60893296b4e01c47253129d2a4fb9e4
tekadabdul12 <tekadabdul12@gmail.com>
2021-10-11 04:35:14 +0700

    problem 9

---
+++ Assets/script/score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    private Text scoreText;
+
+    public static int nilai = 0;
+
+    public static void AddScore(int value)
+    {
+        nilai += value;
+    }
+   
+}
