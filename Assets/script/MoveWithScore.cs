af81dafb5f2bc9f87fb9d4a7f8846bd38d556d85
tekadabdul12 <tekadabdul12@gmail.com>
2021-10-11 00:58:11 +0700

    problem 7

---
+++ Assets/script/MoveWithScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveWithScore : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+
+    public float speed;
+
+    private Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.nilai = 0;
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("point"))
+        {
+            score.AddScore(1);
+            scoreText.text = "Score : " + score.nilai;
+            Destroy(collision.gameObject);
+        }
+    }
+}
