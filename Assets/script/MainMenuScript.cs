24f65f425655dd0422b5bb8182129c9d5f347084
tekadabdul12 <tekadabdul12@gmail.com>
2021-10-11 05:42:20 +0700

    Problem 10

---
+++ Assets/script/MainMenuScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu, PlayMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        PlayMenu.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        MainMenu.SetActive(false);
+        PlayMenu.SetActive(true);
+    }
+
+    public void Level1Button()
+    {
+        SceneManager.LoadScene("p1");
+    }
+    public void Level2Button()
+    {
+        SceneManager.LoadScene("p2");
+    }
+    public void Level3Button()
+    {
+        SceneManager.LoadScene("p3");
+    }
+    public void Level4Button()
+    {
+        SceneManager.LoadScene("p4");
+    }
+    public void Level5Button()
+    {
+        SceneManager.LoadScene("p5");
+    }
+    public void Level6utton()
+    {
+        SceneManager.LoadScene("p6");
+    }
+    public void Level7Button()
+    {
+        SceneManager.LoadScene("p7");
+    }
+    public void Level8Button()
+    {
+        SceneManager.LoadScene("p8");
+    }
+    public void Level9Button()
+    {
+        SceneManager.LoadScene("p9");
+    }
+
+    public void BackButton()
+    {
+        MainMenu.SetActive(true);
+        PlayMenu.SetActive(false);
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("Exit from Game");
+        Application.Quit();
+    }
+}
---
+++ Assets/script/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject point;
+    float randvert, randhoriz, randtotal;
+
+    private void Start()
+    {
+        randtotal = Random.Range(6, 15);
+        for(int i = 0; i < randtotal; i++)
+        {
+            randvert = Random.Range(3.4f, -3.4f);
+            randhoriz = Random.Range(8f, -8f);
+
+            Instantiate(point, new Vector2(randhoriz, randvert),Quaternion.identity);
+        }
+    }
+}
